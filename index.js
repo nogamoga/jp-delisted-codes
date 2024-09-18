c71877a4ca70879e9f52ceff3fcee10158da5baa
nogamoga <55225552+nogamoga@users.noreply.github.com>
2024-09-18 16:47:52 +0900

    引数の順番に問題があり比較に失敗していたのを修正

--- index.js
+++ index.js
@@ -49,12 +49,14 @@ let updateFile = (tableData) => {
       if (fs.existsSync(current_path)) {
         let saved_json = fs.readFileSync(current_path);
         try {
-          if (
-            _.differenceWith(JSON.parse(saved_json), tableData, _.isEqual)
-              .length > 0
-          ) {
-            // Update on mismatch
+          // Update on mismatch
+          let saved_obj = JSON.parse(saved_json.toString());
+          if (saved_obj.length != tableData.length) {
             fs.writeFileSync(current_path, JSON.stringify(tableData));
+          } else {
+            if (_.differenceWith(tableData, saved_obj, _.isEqual).length > 0) {
+              fs.writeFileSync(current_path, JSON.stringify(tableData));
+            }
           }
         } catch (e) {
           fs.writeFileSync(current_path, JSON.stringify(tableData));
